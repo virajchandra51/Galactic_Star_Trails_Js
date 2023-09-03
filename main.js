24bd724b8e79c64d962bc79af6ea00d4267856c0
Viraj Chandra <virajchandra51@gmail.com>
2023-09-03 21:05:28 +0530

    star wars

--- main.js
+++ main.js
@@ -56,10 +56,10 @@ var timer = 0;
 var opacity = 1;
 var speed = 0.0005;
 var colors = [
-"#0952BD",
-"#A5BFF0",
-"#118CD6",
-"#1AAEE8",
+"#eedb00",
+"#fbfb00",
+"#fbfbdd",
+"#333333",
 "#F2E8C9"
 ];
 
@@ -88,7 +88,7 @@ if (isMouseDown === true) {
   // Ease into the new opacity
   var desiredOpacity = 0.01;
   opacity += (desiredOpacity - opacity) * 0.03;
-  c.fillStyle = "rgba(18, 18, 18,"+ opacity +")";
+  c.fillStyle = "rgba(0, 0, 0,"+ opacity +")";
 
   // Ease into the new speed
   var desiredSpeed = 0.012;
@@ -100,7 +100,7 @@ if (isMouseDown === true) {
   // Ease back to the original opacity
   var originalOpacity = 1;
   opacity += (originalOpacity - opacity) * 0.01;
-  c.fillStyle = "rgba(18, 18, 18, " + opacity + ")";
+  c.fillStyle = "rgba(0, 0, 0, " + opacity + ")";
 
   // Ease back to the original speed
   var originalSpeed = 0.001;
